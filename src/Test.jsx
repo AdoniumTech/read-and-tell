e2334dfd352a9b7ccd4cb39179f771f7b03a8dac
bsc-com-04-24 <bsc-com-04-24@unima.ac.mw>
2026-06-13 08:06:43 -0700

    initial commit

---
+++ src/Test.jsx
@@ -0,0 +1,9 @@
+export default function Test() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen sm:bg-white md:bg-gray-900 ">
+            <h1 className="text-4xl font-bold sm:text-red-900 md:text-blue-900 mb-4">Hello! This is the Read And Write project</h1>
+            <p className="text-lg text-gray-600">So excited to be part of this project!</p>
+        </div>
+    )
+}
+       
